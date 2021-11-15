d48f7d488fe166c6999c3ad1cd443a55308c623c
Uduak Essien <acushla4real@gmail.com>
2021-11-15 16:23:11 +0100

    Add slider on homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
 import MainNav from './components/mainNav.component';
+import Slider from './components/slider.component';
 
 function App() {
   return (
     <div>
       <MainNav />
+      {/* TODO: Fix slide text on mobile */}
+      <Slider />
     </div>
   );
 }
---
+++ src/components/slider.component.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import silo_img from '../images/kfm_silo_slide.jpg';
+import plant_img from '../images/kfm_plant_slide.jpg';
+
+export default function Slider() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = selectedIndex => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect} className="home-slider">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={plant_img}
+          alt="KFM Plant"
+        />
+        <Carousel.Caption>
+          <h6 className="text-uppercase">Welcome to Kings Flour Mill</h6>
+          <h1><strong>Production</strong> Driven By Innovation</h1>
+          <p>Our production processes are purely driven by state of the  art technology <br />
+          and innovative industrial manufacturing solutions</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={silo_img}
+          alt="flourmill silo"
+        />
+
+        <Carousel.Caption>
+          <h6 className="text-uppercase">Major Manufacturer</h6>
+          <h1><strong>Capacity</strong> of <strong>500</strong> Metric <br/> Tons per day</h1>
+          <p>We have the capacity and technical know-how to mill varying grades of premium wheat based flour</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
