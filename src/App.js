45b8624d84c9100719708b911727985fdf787ae6
Uduak Essien <acushla4real@gmail.com>
2021-11-18 17:11:40 +0100

    Add routes for pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.scss';
-import { AboutSection } from './components/aboutSection.component';
-import { HealthSection } from './components/healthSection.component';
-import { Mainsection } from './components/mainSection.component';
-import { HeaderContainer } from './container/header.container';
+import Switch from 'react-bootstrap/esm/Switch';
+import { Route } from 'react-router';
+import { HomePage } from './pages/home.page';
+import { AboutPage } from './pages/about.page';
+import { ServicesPage } from './pages/services.page';
+import { NewsPage } from './pages/news.page';
+import { ContactPage } from './pages/contact.page';
 import Footer from './components/footer.component';
 import { Copyright } from './components/copyright.component';
 
 function App() {
   return (
     <div>
-      <HeaderContainer />
-      <Mainsection />
-      <AboutSection />
-      <div className="extra_space"></div>
-      <HealthSection />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/services" component={ServicesPage} />
+        <Route exact path="/news" component={NewsPage} />
+        <Route exact path="/contact" component={ContactPage} />
+      </Switch>
       <Footer />
       <Copyright />
     </div>
