64913b9bf4c7b78fc61cc7a74b0cb4347ce2fc4b
Uduak Essien <acushla4real@gmail.com>
2021-11-19 14:39:51 +0100

    Update about information

--- src/pages/about.page.js
+++ src/pages/about.page.js
@@ -16,9 +16,12 @@ export function AboutPage() {
 
             <Row>
                 <Col className="col-12 col-md-6 mb-4">
-                <h1>About Kings FlourMill</h1>
-            <p>Kings Flour Mill Limited was established in 2019 born out of desires of investing partners to meet the growing needs for superior Wheat based Flour and other Agro-allied products.</p>
-            <p>Kings Flour Mill produces premium quality and value Flour in its category in terms of product performance, yield and profitability</p>
+                    <h1>About Kings FlourMill</h1>
+                    <p>Kings Flour Mill Limited was established in 2019 born out of desires of investing partners to meet the growing needs for superior Wheat based Flour and other Agro-allied products.</p>
+                    <h4>Our Vision</h4>
+                    <p>The vision of the company is to be No. 1 Agric-based company in Africa providing solutions to support players in the Agro-processing value chain.</p>
+                    <h4>Our Mission</h4>
+                    <p>To produce wheat based products using the best global practices and processes; harnessing environmentally friendly technology and people capabilities.</p>
                 </Col>
                 <Col className="col-12 col-md-6 mb-4">
                     <img src={kfmPreview} alt="Kings Flour Mill" className="img-fluid rounded about-img" />
