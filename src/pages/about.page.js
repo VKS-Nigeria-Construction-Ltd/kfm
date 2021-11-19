2716d8d68fc758c2547a2f8b08e3ae4dc2860b0d
Uduak Essien <acushla4real@gmail.com>
2021-11-19 10:58:53 +0100

    Update about us and news page

--- src/pages/about.page.js
+++ src/pages/about.page.js
@@ -1,5 +1,6 @@
-import { Container } from 'react-bootstrap';
-import { Pageheader } from '../container/pageHeader.container'
+import { Container, Row, Col } from 'react-bootstrap';
+import { Pageheader } from '../container/pageHeader.container';
+import kfmPreview from '../images/kfm_preview.jpg';
 
 export function AboutPage() {
     
@@ -8,8 +9,18 @@ export function AboutPage() {
         <>
          <Pageheader title="About Us" />
          <Container className="mt-4 mb-5">
-            <h1>About Kings FlourMill</h1>
-            <p>The company was established for the production of superior quality wheat based flour and other Agro allied products. The initial installed production capacity of the mill is 500 metric tons per day.</p>
+            
+
+            <Row>
+                <Col className="col-12 col-md-6 mb-4">
+                <h1>About Kings FlourMill</h1>
+            <p>Kings Flour Mill Limited was established in 2019 born out of desires of investing partners to meet the growing needs for superior Wheat based Flour and other Agro-allied products.</p>
+            <p>Kings Flour Mill produces premium quality and value Flour in its category in terms of product performance, yield and profitability</p>
+                </Col>
+                <Col className="col-12 col-md-6 mb-4">
+                    <img src={kfmPreview} alt="Kings Flour Mill" className="img-fluid rounded about-img" />
+                </Col>
+            </Row>
          </Container>
          </>
     )
--- src/pages/news.page.js
+++ src/pages/news.page.js
@@ -8,7 +8,7 @@ export function NewsPage() {
          <Pageheader title="News" />
          <Container className="mt-4 mb-5">
             <h1>Latest News</h1>
-            <p>The company was established for the production of superior quality wheat based flour and other Agro allied products. The initial installed production capacity of the mill is 500 metric tons per day.</p>
+            <p>No news at the moment</p>
          </Container>
          </>
     )
